ecfbf955083f78c1f86b045f5d6c583ab2a10c5e
Evelyn <git@evelyn.moe>
2018-10-10 18:12:17 +0100

    Add coloured values to accept/reject

--- src/main/java/moe/evelyn/albatross/rules/GroupEffect.java
+++ src/main/java/moe/evelyn/albatross/rules/GroupEffect.java
@@ -2,5 +2,15 @@ package moe.evelyn.albatross.rules;
 
 public enum GroupEffect
 {
-    ACCEPT, REJECT;
+    ACCEPT("§aACCEPT§r"), REJECT("§cREJECT§r");
+
+    private final String fullColour;
+
+    GroupEffect(String fullColour) {
+        this.fullColour = fullColour;
+    }
+
+    public String getStringColoured() {
+        return this.fullColour;
+    }
 }
